8fca312aee9c47c2cb2f84622a2919fc8c0e92ee
Bryan Long <BryanLong1@gmail.com>
2018-03-21 16:59:13 -0600

    Implementing Delete Toggle - not working yet

--- components/FoodsToAvoid.js
+++ components/FoodsToAvoid.js
@@ -1,52 +1,92 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from "react-native";
-import axios from 'axios';
-import Toggles from './Toggles'
-const API = "https://can-i-eat-that-api.herokuapp.com/api/foods/";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { MKSwitch, MKColor, setTheme } from "react-native-material-kit";
+import axios from "axios";
 
+const API = "https://can-i-eat-that-api.herokuapp.com/api/foods/";
 
 class FoodsToAvoid extends Component {
   constructor(props) {
     super(props);
-      this.state = {
-        foods: []
-      }
-    }
+    this.state = {
+      foods: []
+    };
+  }
 
   componentDidMount = () => {
     this.initData();
   };
 
   initData = () => {
-    axios.get(API)
-    .then(response => this.setState({ foods: response.data }))
-    .catch(err => console.error(err));
+    axios
+      .get(API)
+      .then(response => this.setState({ foods: response.data }))
+      .catch(err => console.error(err));
+  };
+
+  // This function is not working. How to add the "id" param??
+  deleteItem = (id) => {
+    console.log("deleted item", this.state.foods);
+    axios
+      .delete(API + { params: { id: id } })
+      .then(response => response.text())
+      // .then(this.initData)
+      .catch(err => console.error(err));
+
   };
 
   renderFoods = () => {
     console.log("this.state: ", this.state);
-    return this.state.foods.map(food => <View key={food.id} style={styles.container}>
-        <Text>{food.name}</Text>
-        <Toggles />
-       </View>);
+    return this.state.foods.map(food => <View key={food.id} style={styles.card}>
+        <Text style={styles.foodsLabel}>{food.name.charAt(0).toUpperCase() + food.name.slice(1)}</Text>
+        <View style={styles.toggleRow}>
+          <Text style={styles.removeText}>Remove from list</Text>
+          <MKSwitch checked={true} 
+          onPress={this.deleteItem} 
+          // onCheckedChange={this.deleteItem} />
+          />
+        </View>
+      </View>);
   };
 
   render() {
-    return (
-      <View>
-        {this.renderFoods()}
-      </View>
-    );
+    return <View>{this.renderFoods()}</View>;
   }
 }
 
 export default FoodsToAvoid;
 
 const styles = StyleSheet.create({
-    container: {
-      padding: 10
-    },
-    foodsLabel : {
-      fontSize: 20
+  card: {
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: "#d6d7da",
+    margin: 5,
+    borderRadius: 20,
+  },
+  toggleRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  foodsLabel: {
+    paddingTop: 15,
+    fontSize: 20,
+    textAlign: "center"
+  },
+  removeText: {
+    textAlign: "center",
+    color: "#666666",
+    marginTop: 10,
+    marginBottom: 20,
+    fontSize: 12,
+    fontWeight: "300"
   }
-})
+});
+
+//// customize the material design theme
+setTheme({
+  primaryColor: MKColor.Purple,
+  primaryColorRGB: MKColor.RGBPurple,
+  accentColor: MKColor.Amber
+});
